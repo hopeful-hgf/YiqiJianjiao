8d31e81778fb720ac4a6df5807e1ea3ee441065d
hgf <csuhgf@hotmail.com>
2017-10-18 13:43:26 +0800

    添加功能 自动更改点号

--- teqc/split.py
+++ teqc/split.py
@@ -4,12 +4,14 @@ from datetime import datetime,timedelta
 def main():
 
 	dirlist=os.listdir("../")
-	ofile=[it for it in dirlist if it!="teqc"]
+	ofile=[it for it in dirlist if not (it=="teqc" or it[0]=='.') or it[-1]=='/']
 
 ######### CHANGE THE START TIME NEXT LINE ###############
-#	sttime=hhmmss			we think the survey is in a day
+#	sttime= 
+
 	sttime=datetime(2017,10,12,10,00,00)
 	for it in ofile:
+		print(it)
 		os.mkdir('../'+it[0])
 		print(it[0])
 		print(int (it[0])%2)
@@ -38,60 +40,63 @@ def getinfo(name):
 
 
 def odd_station(fname,stt):
+	num=fname[0]
 	endt=stt+timedelta(minutes=40)
-	os.system(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/1.17o')
-	print(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/1.17o')
+	os.system(pre_scr(stt,endt) +num+'N1 ' + ' ../'+fname + ' > ../'+fname[0]+'/1.17o')
+	print(pre_scr(stt,endt)+num+'N1 ' + ' ../'+fname + ' > ../'+fname[0]+'/1.17o')
 
 	stt=endt
 	endt=stt+timedelta(minutes=20)
-	os.system(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/2.17o')
+	os.system(pre_scr(stt,endt)  +num+'E1 '+ ' ../'+ fname + ' > ../'+fname[0]+'/2.17o')
 
 	stt=endt
 	endt=stt+timedelta(minutes=20)
-	os.system(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/3.17o')
+	os.system(pre_scr(stt,endt)  +num+'S1 '+ ' ../'+ fname + ' > ../'+fname[0]+'/3.17o')
 
 	stt=endt
 	endt=stt+timedelta(minutes=20)
-	os.system(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/4.17o')
+	os.system(pre_scr(stt,endt) +num+'W1 ' + ' ../'+ fname + ' > ../'+fname[0]+'/4.17o')
 
 	stt=endt
 	endt=stt+timedelta(minutes=40)
-	os.system(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/5.17o')
+	os.system(pre_scr(stt,endt)  +num+'N2 '+ ' ../'+ fname + ' > ../'+fname[0]+'/5.17o')
 
 	stt=endt
 	endt=stt+timedelta(minutes=60)
-	os.system(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/6.17o')
+	os.system(pre_scr(stt,endt) +num+'N3 ' + ' ../'+ fname + ' > ../'+fname[0]+'/6.17o')
 
 
 def even_station(fname,stt):
+	num=fname[0]
 	endt=stt+timedelta(minutes=40)
-	os.system(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/1.17o')
+	os.system(pre_scr(stt,endt) +num+'N1 ' + ' ../'+fname + ' > ../'+fname[0]+'/1.17o')
+	print(pre_scr(stt,endt)+num+'N1 ' + ' ../'+fname + ' > ../'+fname[0]+'/1.17o')
 
 	stt=endt
 	endt=stt+timedelta(minutes=60)
-	os.system(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/2.17o')
+	os.system(pre_scr(stt,endt)  +num+'N2 '+ ' ../'+ fname + ' > ../'+fname[0]+'/2.17o')
 
 	stt=endt
 	endt=stt+timedelta(minutes=40)
-	os.system(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/3.17o')
+	os.system(pre_scr(stt,endt)  +num+'N3 '+ ' ../'+ fname + ' > ../'+fname[0]+'/3.17o')
 
 	stt=endt
 	endt=stt+timedelta(minutes=20)
-	os.system(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/4.17o')
+	os.system(pre_scr(stt,endt) +num+'E1 ' + ' ../'+ fname + ' > ../'+fname[0]+'/4.17o')
 
 	stt=endt
 	endt=stt+timedelta(minutes=20)
-	os.system(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/5.17o')
+	os.system(pre_scr(stt,endt)  +num+'S1 '+ ' ../'+ fname + ' > ../'+fname[0]+'/5.17o')
 
 	stt=endt
 	endt=stt+timedelta(minutes=20)
-	os.system(pre_scr(stt,endt)+ fname + ' > ../'+fname[0]+'/6.17o')
+	os.system(pre_scr(stt,endt) +num+'W1 ' + ' ../'+ fname + ' > ../'+fname[0]+'/6.17o')
 
 
 def pre_scr(stt,endt):
 	strs=stt.strftime('%Y,%m,%d,%H,%M,%S')
 	stre=endt.strftime('%Y,%m,%d,%H,%M,%S')
-	return 'teqc -st ' + strs + ' -e ' +stre + ' ../'
+	return 'teqc -st ' + strs + ' -e ' +stre+ ' -O.mo 0'
 
 if __name__=='__main__':
 	main()
